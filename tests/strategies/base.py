6cc8cd1e8af81279339914e4e0388ac1b05be060
Azat Ibrakov <azatibrakov@gmail.com>
2020-01-29 23:30:42 +0300

    Complete strategies

--- tests/strategies/base.py
+++ tests/strategies/base.py
@@ -1,7 +1,9 @@
+import sys
 from decimal import Decimal
 from fractions import Fraction
-from functools import partial
 from operator import ne
+from typing import (Optional,
+                    SupportsFloat)
 
 from hypothesis import strategies
 
@@ -14,16 +16,68 @@ from tests.utils import (Strategy,
 
 MAX_FLOAT = 1.e10
 MIN_FLOAT = -MAX_FLOAT
-scalars_strategies_factories = {Decimal: partial(strategies.decimals,
-                                                 min_value=MIN_FLOAT,
-                                                 max_value=MAX_FLOAT,
-                                                 allow_nan=False,
-                                                 allow_infinity=False),
-                                float: partial(strategies.floats,
-                                               min_value=MIN_FLOAT,
-                                               max_value=MAX_FLOAT,
-                                               allow_nan=False,
-                                               allow_infinity=False),
+
+
+def to_decimals(*,
+                min_value: Optional[Scalar] = MIN_FLOAT,
+                max_value: Optional[Scalar] = MAX_FLOAT,
+                allow_nan: bool = False,
+                allow_infinity: bool = False) -> Strategy:
+    return (strategies.decimals(min_value=min_value,
+                                max_value=max_value,
+                                allow_nan=allow_nan,
+                                allow_infinity=allow_infinity)
+            .map(to_digits_count))
+
+
+def to_floats(*,
+              min_value: Optional[Scalar] = MIN_FLOAT,
+              max_value: Optional[Scalar] = MAX_FLOAT,
+              allow_nan: bool = False,
+              allow_infinity: bool = False) -> Strategy:
+    return (strategies.floats(min_value=min_value,
+                              max_value=max_value,
+                              allow_nan=allow_nan,
+                              allow_infinity=allow_infinity)
+            .map(to_digits_count))
+
+
+def to_digits_count(number: Scalar,
+                    *,
+                    max_digits_count: int = sys.float_info.dig) -> Scalar:
+    decimal = to_decimal(number).normalize()
+    _, significant_digits, exponent = decimal.as_tuple()
+    significant_digits_count = len(significant_digits)
+    if exponent < 0:
+        fixed_digits_count = (1 - exponent
+                              if exponent <= -significant_digits_count
+                              else significant_digits_count)
+    else:
+        fixed_digits_count = exponent + significant_digits_count
+    if fixed_digits_count <= max_digits_count:
+        return number
+    whole_digits_count = max(significant_digits_count + exponent, 0)
+    if whole_digits_count:
+        whole_digits_offset = max(whole_digits_count - max_digits_count, 0)
+        decimal /= 10 ** whole_digits_offset
+        whole_digits_count -= whole_digits_offset
+    else:
+        decimal *= 10 ** (-exponent - significant_digits_count)
+        whole_digits_count = 1
+    decimal = round(decimal, max(max_digits_count - whole_digits_count, 0))
+    return type(number)(str(decimal))
+
+
+def to_decimal(number: SupportsFloat) -> Decimal:
+    if isinstance(number, Decimal):
+        return number
+    elif not isinstance(number, (int, float)):
+        number = float(number)
+    return Decimal(number)
+
+
+scalars_strategies_factories = {Decimal: to_decimals,
+                                float: to_floats,
                                 Fraction: strategies.fractions,
                                 int: strategies.integers}
 scalars_strategies = strategies.sampled_from(
