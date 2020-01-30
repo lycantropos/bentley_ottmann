8b3d30fb7ca3f38c92c66d294b5c5de9a2a28250
Azat Ibrakov <azatibrakov@gmail.com>
2020-01-30 14:55:45 +0300

    Complete test for 'segments_intersections' function

--- tests/base_tests/test_segments_intersections.py
+++ tests/base_tests/test_segments_intersections.py
@@ -7,9 +7,6 @@ from bentley_ottmann.base import segments_intersections
 from bentley_ottmann.linear import (Segment,
                                     find_intersections)
 from bentley_ottmann.point import Point
-from tests.utils import (reverse_point_coordinates,
-                         reverse_segment,
-                         reverse_segment_coordinates)
 from . import strategies
 
 
@@ -59,31 +56,3 @@ def test_step(segments: List[Segment]) -> None:
                for point, intersections in next_result.items()
                for segment_id, next_segment_id in (intersections
                                                    - result.get(point, set())))
-
-
-@given(strategies.segments_lists)
-def test_reversed(segments: List[Segment]) -> None:
-    result = segments_intersections(segments)
-
-    assert result == segments_intersections(segments[::-1])
-
-
-@given(strategies.segments_lists)
-def test_reversed_endpoints(segments: List[Segment]) -> None:
-    result = segments_intersections(segments)
-
-    assert result == segments_intersections([reverse_segment(segment)
-                                             for segment in segments])
-
-
-@given(strategies.segments_lists)
-def test_reversed_coordinates(segments: List[Segment]) -> None:
-    result = segments_intersections(segments)
-
-    reversed_result = segments_intersections(
-            [reverse_segment_coordinates(segment) for segment in segments])
-
-    assert all(reverse_point_coordinates(point) in result.keys()
-               for point in reversed_result.keys())
-    assert all(intersections in result.values()
-               for intersections in reversed_result.values())
