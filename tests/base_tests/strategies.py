46da94da15d4b46b4b330fdee4e3d2b73b78770f
Azat Ibrakov <azatibrakov@gmail.com>
2020-01-30 19:45:40 +0300

    Complete test for 'edges_intersect' function

--- tests/base_tests/strategies.py
+++ tests/base_tests/strategies.py
@@ -2,18 +2,16 @@ from functools import partial
 
 from hypothesis import strategies
 
-from bentley_ottmann.base import _vertices_to_edges
 from tests.strategies import (points_strategies,
                               segments_strategies)
 
-to_non_empty_lists = partial(strategies.lists,
-                             min_size=1)
-edges_lists = (points_strategies
-               .flatmap(strategies.lists)
-               .map(_vertices_to_edges))
-non_empty_edges_lists = (points_strategies
-                         .flatmap(to_non_empty_lists)
-                         .map(_vertices_to_edges))
+vertices_lists = (points_strategies
+                  .flatmap(strategies.lists))
+non_empty_vertices_lists = (points_strategies
+                            .flatmap(partial(strategies.lists,
+                                             min_size=3)))
 segments_lists = segments_strategies.flatmap(strategies.lists)
-empty_edges_lists = empty_segments_lists = strategies.builds(list)
-non_empty_segments_lists = segments_strategies.flatmap(to_non_empty_lists)
+empty_vertices_lists = empty_segments_lists = strategies.builds(list)
+non_empty_segments_lists = (segments_strategies
+                            .flatmap(partial(strategies.lists,
+                                             min_size=1)))
--- tests/base_tests/test_edges_intersect.py
+++ tests/base_tests/test_edges_intersect.py
@@ -1,62 +1,79 @@
+from itertools import (chain,
+                       combinations)
 from typing import List
 
 from hypothesis import given
 
-from bentley_ottmann.base import edges_intersect
-from bentley_ottmann.linear import (Segment,
-                                    find_intersections)
-from tests.utils import (reverse_segment,
-                         reverse_segment_coordinates)
+from bentley_ottmann.base import (_vertices_to_edges,
+                                  edges_intersect)
+from bentley_ottmann.linear import (Segment, SegmentsRelationship,
+                                    find_intersections,
+                                    to_segments_relationship)
+from bentley_ottmann.point import Point
+from tests.utils import reverse_point_coordinates
 from . import strategies
 
 
-@given(strategies.edges_lists)
-def test_basic(edges: List[Segment]) -> None:
-    result = edges_intersect(edges)
+@given(strategies.vertices_lists)
+def test_basic(vertices: List[Point]) -> None:
+    result = edges_intersect(vertices)
 
     assert isinstance(result, bool)
 
 
-@given(strategies.empty_edges_lists)
-def test_base_case(edges: List[Segment]) -> None:
-    result = edges_intersect(edges)
+@given(strategies.empty_vertices_lists)
+def test_base_case(vertices: List[Point]) -> None:
+    result = edges_intersect(vertices)
 
     assert not result
 
 
-@given(strategies.non_empty_edges_lists)
-def test_step(edges: List[Segment]) -> None:
-    first_segment, *rest_edges = edges
+@given(strategies.non_empty_vertices_lists)
+def test_step(vertices: List[Point]) -> None:
+    first_vertex, *rest_vertices = vertices
 
-    result = edges_intersect(rest_edges)
-    next_result = edges_intersect(edges)
+    result = edges_intersect(rest_vertices)
+    next_result = edges_intersect(vertices)
 
-    assert next_result is (result
-                           or (len(rest_edges) > 2
-                               and bool(find_intersections(rest_edges[0],
-                                                           rest_edges[-1])))
-                           or any(find_intersections(first_segment, edge)
-                                  for edge in rest_edges[1:-1]))
+    first_edge, last_edge = (Segment(first_vertex, rest_vertices[0]),
+                             Segment(rest_vertices[-1], first_vertex))
+    rest_edges = _vertices_to_edges(rest_vertices)
+    assert (next_result
+            is (result
+                and len(rest_vertices) > 2
+                and (any(find_intersections(rest_edges[index],
+                                            rest_edges[other_index])
+                         for index in range(len(rest_edges) - 1)
+                         for other_index in chain(
+                            range(index - 1),
+                            range(index + 2, len(rest_edges) - 1)))
+                     or any(to_segments_relationship(edge, other_edge)
+                            is SegmentsRelationship.OVERLAP
+                            for edge, other_edge in combinations(
+                                    rest_edges[:-1], 2)))
+                or any(find_intersections(first_edge, edge)
+                       for edge in rest_edges[1:-1])
+                or any(find_intersections(last_edge, edge)
+                       for edge in rest_edges[:-2])
+                or len(rest_vertices) > 1
+                and (to_segments_relationship(first_edge, rest_edges[0])
+                     is SegmentsRelationship.OVERLAP
+                     or to_segments_relationship(first_edge, last_edge)
+                     is SegmentsRelationship.OVERLAP
+                     or to_segments_relationship(last_edge, rest_edges[0])
+                     is SegmentsRelationship.OVERLAP)))
 
 
-@given(strategies.edges_lists)
-def test_reversed(edges: List[Segment]) -> None:
-    result = edges_intersect(edges)
+@given(strategies.vertices_lists)
+def test_reversed(vertices: List[Point]) -> None:
+    result = edges_intersect(vertices)
 
-    assert result is edges_intersect(edges[::-1])
-
-
-@given(strategies.edges_lists)
-def test_reversed_endpoints(edges: List[Segment]) -> None:
-    result = edges_intersect(edges)
-
-    assert result is edges_intersect([reverse_segment(edge)
-                                      for edge in edges])
+    assert result is edges_intersect(vertices[::-1])
 
 
-@given(strategies.edges_lists)
-def test_reversed_coordinates(edges: List[Segment]) -> None:
-    result = edges_intersect(edges)
+@given(strategies.vertices_lists)
+def test_reversed_coordinates(vertices: List[Point]) -> None:
+    result = edges_intersect(vertices)
 
-    assert result is edges_intersect([reverse_segment_coordinates(edge)
-                                      for edge in edges])
+    assert result is edges_intersect([reverse_point_coordinates(vertex)
+                                      for vertex in vertices])
