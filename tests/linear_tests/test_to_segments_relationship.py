f1f1094e2b1a3a3c5b70615ba73149451cf0b306
Azat Ibrakov <azatibrakov@gmail.com>
2020-01-31 01:35:06 +0300

    Add test for 'to_segments_relationship' function

---
+++ tests/linear_tests/test_to_segments_relationship.py
@@ -0,0 +1,33 @@
+from typing import Tuple
+
+from hypothesis import given
+
+from bentley_ottmann.linear import (Segment,
+                                    SegmentsRelationship,
+                                    to_segments_relationship)
+from . import strategies
+
+
+@given(strategies.segments_pairs)
+def test_basic(segments_pair: Tuple[Segment, Segment]) -> None:
+    first_segment, second_segment = segments_pair
+
+    result = to_segments_relationship(first_segment, second_segment)
+
+    assert isinstance(result, SegmentsRelationship)
+
+
+@given(strategies.segments_pairs)
+def test_commutativity(segments_pair: Tuple[Segment, Segment]) -> None:
+    first_segment, second_segment = segments_pair
+
+    result = to_segments_relationship(first_segment, second_segment)
+
+    assert result is to_segments_relationship(second_segment, first_segment)
+
+
+@given(strategies.segments)
+def test_self(segment: Segment) -> None:
+    result = to_segments_relationship(segment, segment)
+
+    assert result is SegmentsRelationship.OVERLAP
