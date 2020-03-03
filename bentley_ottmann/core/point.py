f75db94958bc3e4a8fc6c6295a6071d25565ee80
Azat Ibrakov <azatibrakov@gmail.com>
2020-03-03 18:46:07 +0300

    Complete type annotations

--- bentley_ottmann/core/point.py
+++ bentley_ottmann/core/point.py
@@ -1,11 +1,11 @@
 from fractions import Fraction
-from numbers import Real
-from typing import Type
+from numbers import (Rational,
+                     Real)
 
 from robust.hints import Point as RealPoint
 
-from bentley_ottmann.hints import (Point,
-                                   Scalar)
+from bentley_ottmann.hints import (Base,
+                                   Point)
 
 
 def is_real_point(point: Point) -> bool:
@@ -18,14 +18,14 @@ def to_real_point(point: Point) -> RealPoint:
     return float(x), float(y)
 
 
-def to_scalar_point(point: Point, coordinate_type: Type[Scalar]) -> Point:
+def to_scalar_point(point: Point, base: Base) -> Point:
     x, y = point
-    return (coordinate_type(x.numerator) / x.denominator
-            if isinstance(x, Fraction)
-            else coordinate_type(x),
-            coordinate_type(y.numerator) / y.denominator
-            if isinstance(y, Fraction)
-            else coordinate_type(y))
+    return (base(x.numerator) / x.denominator
+            if isinstance(x, Rational)
+            else base(x),
+            base(y.numerator) / y.denominator
+            if isinstance(y, Rational)
+            else base(y))
 
 
 def to_rational_point(point: Point) -> Point:
