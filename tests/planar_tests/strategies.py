7934961764793931f4471e01a46eee739d2fe223
Ibrakov Azat <azatibrakov@gmail.com>
2020-03-04 21:56:40 +0300

    Complete strategies

--- tests/planar_tests/strategies.py
+++ tests/planar_tests/strategies.py
@@ -5,12 +5,12 @@ from hypothesis import strategies
 from tests.strategies import (points_strategies,
                               segments_strategies)
 
-vertices_lists = points_strategies.flatmap(strategies.lists)
-non_empty_vertices_lists = (points_strategies
-                            .flatmap(partial(strategies.lists,
-                                             min_size=3)))
+contours = points_strategies.flatmap(strategies.lists)
+non_empty_contours = (points_strategies
+                      .flatmap(partial(strategies.lists,
+                                       min_size=3)))
 segments_lists = segments_strategies.flatmap(strategies.lists)
-empty_vertices_lists = empty_segments_lists = strategies.builds(list)
+empty_contours = empty_segments_lists = strategies.builds(list)
 non_empty_segments_lists = (segments_strategies
                             .flatmap(partial(strategies.lists,
                                              min_size=1)))
