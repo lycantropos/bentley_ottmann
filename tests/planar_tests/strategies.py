0a207526b4db599249f0d180986c57f4bb13e165
Azat Ibrakov <azatibrakov@gmail.com>
2020-05-13 12:56:42 +0300

    Complete test for 'segments_intersect' function

--- tests/planar_tests/strategies.py
+++ tests/planar_tests/strategies.py
@@ -1,9 +1,13 @@
 from functools import partial
+from itertools import repeat
+from typing import Tuple
 
 from hypothesis import strategies
 
+from bentley_ottmann.hints import Point
 from tests.strategies import (points_strategies,
                               segments_strategies)
+from tests.utils import Strategy
 
 contours = points_strategies.flatmap(strategies.lists)
 non_empty_contours = (points_strategies
@@ -14,3 +18,15 @@ empty_contours = empty_segments_lists = strategies.builds(list)
 non_empty_segments_lists = (segments_strategies
                             .flatmap(partial(strategies.lists,
                                              min_size=1)))
+
+
+def points_to_degenerate_segments(points: Strategy[Point]
+                                  ) -> Strategy[Tuple[Point, Point]]:
+    return (points.map(partial(repeat,
+                               times=2))
+            .map(tuple))
+
+
+degenerate_segments = points_strategies.flatmap(points_to_degenerate_segments)
+degenerate_segments_lists = strategies.lists(degenerate_segments,
+                                             min_size=1)
