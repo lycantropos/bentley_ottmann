d2504d94429f46e9bc225f1ac3b43dc35168cada
Azat Ibrakov <azatibrakov@gmail.com>
2020-05-19 22:25:23 +0300

    Rename function: 'segments_overlap_or_cross' -> 'segments_cross_or_overlap'

--- tests/planar_tests/test_segments_cross_or_overlap.py
+++ tests/planar_tests/test_segments_cross_or_overlap.py
@@ -6,7 +6,7 @@ from hypothesis import given
 from bentley_ottmann.core.linear import (SegmentsRelationship,
                                          segments_relationship)
 from bentley_ottmann.hints import Segment
-from bentley_ottmann.planar import segments_overlap_or_cross
+from bentley_ottmann.planar import segments_cross_or_overlap
 from tests.utils import (reverse_segment,
                          reverse_segment_coordinates)
 from . import strategies
@@ -14,14 +14,14 @@ from . import strategies
 
 @given(strategies.segments_lists)
 def test_basic(segments: List[Segment]) -> None:
-    result = segments_overlap_or_cross(segments)
+    result = segments_cross_or_overlap(segments)
 
     assert isinstance(result, bool)
 
 
 @given(strategies.empty_segments_lists)
 def test_base_case(segments: List[Segment]) -> None:
-    result = segments_overlap_or_cross(segments)
+    result = segments_cross_or_overlap(segments)
 
     assert not result
 
@@ -30,8 +30,8 @@ def test_base_case(segments: List[Segment]) -> None:
 def test_step(segments: List[Segment]) -> None:
     first_segment, *rest_segments = segments
 
-    result = segments_overlap_or_cross(rest_segments)
-    next_result = segments_overlap_or_cross(segments)
+    result = segments_cross_or_overlap(rest_segments)
+    next_result = segments_cross_or_overlap(segments)
 
     assert (next_result
             is (result
@@ -43,29 +43,29 @@ def test_step(segments: List[Segment]) -> None:
 
 @given(strategies.segments_lists)
 def test_reversed(segments: List[Segment]) -> None:
-    result = segments_overlap_or_cross(segments)
+    result = segments_cross_or_overlap(segments)
 
-    assert result is segments_overlap_or_cross(segments[::-1])
+    assert result is segments_cross_or_overlap(segments[::-1])
 
 
 @given(strategies.segments_lists)
 def test_reversed_endpoints(segments: List[Segment]) -> None:
-    result = segments_overlap_or_cross(segments)
+    result = segments_cross_or_overlap(segments)
 
-    assert result is segments_overlap_or_cross([reverse_segment(segment)
+    assert result is segments_cross_or_overlap([reverse_segment(segment)
                                                 for segment in segments])
 
 
 @given(strategies.segments_lists)
 def test_reversed_coordinates(segments: List[Segment]) -> None:
-    result = segments_overlap_or_cross(segments)
+    result = segments_cross_or_overlap(segments)
 
     assert (result
-            is segments_overlap_or_cross([reverse_segment_coordinates(segment)
+            is segments_cross_or_overlap([reverse_segment_coordinates(segment)
                                           for segment in segments]))
 
 
 @given(strategies.degenerate_segments_lists)
 def test_degenerate_segments(segments: List[Segment]) -> None:
     with pytest.raises(ValueError):
-        segments_overlap_or_cross(segments)
+        segments_cross_or_overlap(segments)
