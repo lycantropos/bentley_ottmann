974a2dc34d1d86fc99f682bcda0ab5ccc66584d9
Azat Ibrakov <azatibrakov@gmail.com>
2020-12-18 08:23:48 +0300

    Add fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,15 @@
 import os
 
+import pytest
+from ground.linear import (to_segment_containment_checker,
+                           to_segments_intersector,
+                           to_segments_relater)
 from hypothesis import (HealthCheck,
                         settings)
+
+from tests.hints import (SegmentContainmentChecker,
+                         SegmentsIntersector,
+                         SegmentsRelater)
 
 on_travis_ci = os.getenv('CI', False)
 on_azure_pipelines = os.getenv('TF_BUILD', False)
@@ -12,3 +20,18 @@ settings.register_profile('default',
                           deadline=None,
                           suppress_health_check=[HealthCheck.filter_too_much,
                                                  HealthCheck.too_slow])
+
+
+@pytest.fixture(scope='session')
+def segment_containment_checker() -> SegmentContainmentChecker:
+    return to_segment_containment_checker()
+
+
+@pytest.fixture(scope='session')
+def segments_intersector() -> SegmentsIntersector:
+    return to_segments_intersector()
+
+
+@pytest.fixture(scope='session')
+def segments_relater() -> SegmentsRelater:
+    return to_segments_relater()
