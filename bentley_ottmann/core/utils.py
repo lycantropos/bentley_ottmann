c67ca3bb896c7b888d457fc932ef674c7b3d1222
Azat Ibrakov <azatibrakov@gmail.com>
2021-01-09 18:13:16 +0300

    Complete 'merge_ids' function

--- bentley_ottmann/core/utils.py
+++ bentley_ottmann/core/utils.py
@@ -1,11 +1,13 @@
 from functools import partial
-from itertools import combinations
+from heapq import merge
+from itertools import (combinations,
+                       groupby)
 from typing import Sequence
 
 
 def merge_ids(left_ids: Sequence[int],
               right_ids: Sequence[int]) -> Sequence[int]:
-    return sorted({*left_ids, *right_ids})
+    return [key for key, _ in groupby(merge(left_ids, right_ids))]
 
 
 to_pairs_combinations = partial(combinations,
