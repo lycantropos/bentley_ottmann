8d6479ac16dedd2cd8bab1ef3dba945400cebc7f
Azat Ibrakov <azatibrakov@gmail.com>
2021-04-02 19:55:19 +0300

    Complete tests configurations

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,19 @@
+import os
+import platform
+
 import pytest
 from ground.base import (Context,
                          get_context)
 from hypothesis import (HealthCheck,
                         settings)
 
+on_azure_pipelines = bool(os.getenv('TF_BUILD', False))
+is_pypy = platform.python_implementation() == 'PyPy'
 settings.register_profile('default',
-                          deadline=None,
+                          max_examples=(settings.default.max_examples
+                                        // (1 + 3 * is_pypy)
+                                        if on_azure_pipelines
+                                        else settings.default.max_examples),
                           suppress_health_check=[HealthCheck.filter_too_much,
                                                  HealthCheck.too_slow])
 
