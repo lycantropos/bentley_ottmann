ecb0cf9a9a59f5bfcd001306c437aacf5b912177
Azat Ibrakov <azatibrakov@gmail.com>
2021-04-02 19:33:13 +0300

    Complete members order for 'events_queue' module

--- bentley_ottmann/core/events_queue.py
+++ bentley_ottmann/core/events_queue.py
@@ -11,40 +11,6 @@ from .event import (Event,
 from .sweep_line import SweepLine
 
 
-class EventsQueueKey:
-    __slots__ = 'event',
-
-    def __init__(self, event: Event) -> None:
-        self.event = event
-
-    __repr__ = generate_repr(__init__)
-
-    def __lt__(self, other: 'EventsQueueKey') -> bool:
-        """
-        Checks if the event should be processed before the other.
-        """
-        event, other_event = self.event, other.event
-        start_x, start_y = event.start.x, event.start.y
-        other_start_x, other_start_y = other_event.start.x, other_event.start.y
-        if start_x != other_start_x:
-            # different x-coordinate,
-            # the event with lower x-coordinate is processed first
-            return start_x < other_start_x
-        elif start_y != other_start_y:
-            # different starts, but same x-coordinate,
-            # the event with lower y-coordinate is processed first
-            return start_y < other_start_y
-        elif event.is_left is not other_event.is_left:
-            # same start, but one is a left endpoint
-            # and the other is a right endpoint,
-            # the right endpoint is processed first
-            return not event.is_left
-        else:
-            # same start,
-            # both events are left endpoints or both are right endpoints
-            return event.end < other_event.end
-
-
 class EventsQueue:
     @classmethod
     def from_segments(cls,
@@ -150,3 +116,37 @@ class EventsQueue:
                              'with both endpoints being: {}.'
                              .format(event.start))
         self._queue.push(event)
+
+
+class EventsQueueKey:
+    __slots__ = 'event',
+
+    def __init__(self, event: Event) -> None:
+        self.event = event
+
+    __repr__ = generate_repr(__init__)
+
+    def __lt__(self, other: 'EventsQueueKey') -> bool:
+        """
+        Checks if the event should be processed before the other.
+        """
+        event, other_event = self.event, other.event
+        start_x, start_y = event.start.x, event.start.y
+        other_start_x, other_start_y = other_event.start.x, other_event.start.y
+        if start_x != other_start_x:
+            # different x-coordinate,
+            # the event with lower x-coordinate is processed first
+            return start_x < other_start_x
+        elif start_y != other_start_y:
+            # different starts, but same x-coordinate,
+            # the event with lower y-coordinate is processed first
+            return start_y < other_start_y
+        elif event.is_left is not other_event.is_left:
+            # same start, but one is a left endpoint
+            # and the other is a right endpoint,
+            # the right endpoint is processed first
+            return not event.is_left
+        else:
+            # same start,
+            # both events are left endpoints or both are right endpoints
+            return event.end < other_event.end
