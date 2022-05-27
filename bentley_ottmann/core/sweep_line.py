84d66b4efd52f0af25d01f6911042eb0632ff116
Azat Ibrakov <azatibrakov@gmail.com>
2022-05-27 14:08:24 +0700

    Complete 'SweepLineKey.__lt__' magic method

--- bentley_ottmann/core/sweep_line.py
+++ bentley_ottmann/core/sweep_line.py
@@ -80,22 +80,19 @@ class SweepLineKey:
                 # other segment fully lies on one side
                 return other_start_orientation is Orientation.COUNTERCLOCKWISE
             # segments are collinear
-            elif start_x == other_start_x:
+            elif start_y == other_start_y:
                 end_x, end_y = end.x, end.y
                 other_end_x, other_end_y = other_end.x, other_end.y
-                if start_y != other_start_y:
-                    # segments are vertical
-                    return start_y < other_start_y
+                if start_x != other_start_x:
+                    return start_x < other_start_x
                 # segments have same start
                 elif end_y != other_end_y:
                     return end_y < other_end_y
                 else:
+                    # segments are horizontal
                     return end_x < other_end_x
-            elif start_y != other_start_y:
-                return start_y < other_start_y
             else:
-                # segments are horizontal
-                return start_x < other_start_x
+                return start_y < other_start_y
         start_orientation = self.orienteer(other_start, other_end, start)
         end_orientation = self.orienteer(other_start, other_end, end)
         if start_orientation is end_orientation:
