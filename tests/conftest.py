e2c78c643b8617b3df6fc564563048a18b57e3b6
Azat Ibrakov <azatibrakov@gmail.com>
2022-06-15 03:36:19 +0700

    Complete tests configurations

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,5 +1,6 @@
 import os
 import platform
+import time
 from datetime import timedelta
 
 import pytest
@@ -14,11 +15,29 @@ max_examples = (-(-settings.default.max_examples // 4)
                 if is_pypy and on_ci
                 else settings.default.max_examples)
 settings.register_profile('default',
-                          deadline=(timedelta(hours=1) / max_examples
-                                    if on_ci
-                                    else None),
+                          deadline=None,
                           max_examples=max_examples,
                           suppress_health_check=[HealthCheck.too_slow])
+
+if on_ci:
+    time_left = timedelta(hours=1)
+
+
+    @pytest.hookimpl(tryfirst=True)
+    def pytest_runtest_call(item: pytest.Item) -> None:
+        set_deadline = settings(deadline=time_left / max_examples)
+        item.obj = set_deadline(item.obj)
+
+
+    @pytest.fixture(scope='function', autouse=True)
+    def time_function_call() -> None:
+        start = time.monotonic()
+        try:
+            yield
+        finally:
+            duration = timedelta(seconds=time.monotonic() - start)
+            global time_left
+            time_left = max(duration, time_left) - duration
 
 
 @pytest.hookimpl(trylast=True)
