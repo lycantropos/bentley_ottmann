6d06295397fa0949b9c254b88448830df6d4d934
Azat Ibrakov <azatibrakov@gmail.com>
2023-05-12 23:30:45 +0200

    Complete metadata

--- setup.py
+++ setup.py
@@ -1,37 +1,9 @@
-from pathlib import Path
-
 from setuptools import (find_packages,
                         setup)
-
-import bentley_ottmann
 
 project_base_url = 'https://github.com/lycantropos/bentley_ottmann/'
 
 
-def read_file(path_string: str) -> str:
-    return Path(path_string).read_text(encoding='utf-8')
-
-
-setup(name=bentley_ottmann.__name__,
-      packages=find_packages(exclude=('tests', 'tests.*')),
-      version=bentley_ottmann.__version__,
-      description=bentley_ottmann.__doc__,
-      long_description=read_file('README.md'),
-      long_description_content_type='text/markdown',
-      author='Azat Ibrakov',
-      author_email='azatibrakov@gmail.com',
-      classifiers=[
-          'License :: OSI Approved :: MIT License',
-          'Programming Language :: Python :: 3.7',
-          'Programming Language :: Python :: 3.8',
-          'Programming Language :: Python :: 3.9',
-          'Programming Language :: Python :: 3.10',
-          'Programming Language :: Python :: 3.11',
-          'Programming Language :: Python :: Implementation :: CPython',
-          'Programming Language :: Python :: Implementation :: PyPy',
-      ],
-      license='MIT License',
+setup(packages=find_packages(exclude=('tests', 'tests.*')),
       url=project_base_url,
-      download_url=project_base_url + 'archive/master.zip',
-      python_requires='>=3.7',
-      install_requires=read_file('requirements.txt'))
+      download_url=project_base_url + 'archive/master.zip')
